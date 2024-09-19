ed100183a4ac06515b1346226d7f3f0332f8785b
Sunday Ajakaiye <marcusitovie@gmail.com>
2024-09-19 02:17:45 +0100

    Add flask_login to manage user sessions
    Remove primary_key=True from sqlalchemy username field, id inherited from
    BaseModel class is already a primary, so we cannot have 2
    primary key in a table.

--- models/user.py
+++ models/user.py
@@ -6,12 +6,13 @@ Contains the class DBStorage
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from flask_login import UserMixin
 
 
-class User(BaseModel, Base):
+class User(UserMixin, BaseModel, Base):
     """Create user table in the database"""
     if models.storage_type == "db":
         __tablename__ = 'users'
-        username = Column(String(45), nullable=False, primary_key=True)
-        email = Column(String(45), nullable=False)
+        username = Column(String(45), nullable=False)
+        email = Column(String(45), unique=True, nullable=False)
         password_hash = Column(String(60), nullable=False)
